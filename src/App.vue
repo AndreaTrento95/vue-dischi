8bd574a517da553c264b333d49bd8208bfa14b7a
AndreaTrento95 <trentora95@gmail.com>
2021-05-24 16:49:41 +0200

    svolgimento

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <div id="app">
-    
+    <div class="container">
+      <h1 class="text-center mt-5">Vue Dischi</h1>
+      <div class="row">
+        <Disc
+          v-for="(disc, index) in discs"
+          :key= 'index'
+          :disc='disc'
+         />
+        
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+
+import axios from 'axios';
+import Disc from '@/components/Disc.vue';
 
 export default {
   name: "App",
   components: {
+    Disc,
     
   },
+  data(){
+    return{
+      axios,
+      discs: [],
+
+    }
+  },
+  created(){
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then(res => {
+      this.discs = res.data;
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
 };
 </script>
 
---
+++ src/components/Disc.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="col-sm-4 col-md-4 col-lg-3">
+
+      <img :src="disc.poster" :alt="disc.title">
+      <h2>{{ disc.title }}</h2>
+      <h4>{{ disc.author }}</h4>
+      <h5>{{ disc.year }}</h5>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Disc',
+    props:{
+        disc: Object
+    }
+}
+</script>
+
+<style>
+
+</style>
